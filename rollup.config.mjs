7a795fc7e4452057dc75700359fed5ee10255b37
Teagan glenn <that@teagantotally.rocks>
2026-01-18 09:20:16 -0700

    chore: initial node project files

---
+++ rollup.config.mjs
@@ -0,0 +1,24 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import typescript from "rollup-plugin-typescript2";
+import terser from "@rollup/plugin-terser";
+import { babel } from "@rollup/plugin-babel";
+
+const dev = process.env.ROLLUP_WATCH;
+
+export default [
+  {
+    input: "js/panel/main.ts",
+    output: {
+      file: "custom_components/intentsity/panel.js",
+      format: "es",
+    },
+    plugins: [
+      nodeResolve(),
+      json(),
+      typescript(),
+      babel({ babelHelpers: 'bundled', exclude: "node_modules/**" }),
+      !dev && terser({ format: { comments: false } }),
+    ],
+  },
+];
